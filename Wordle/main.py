085938b629a7d70f8cf86edc295f140814f2e28c
jimeeeb08 <129323337+jimeeeb08@users.noreply.github.com>
2026-06-25 13:10:10 -0600

    INICIO PROYECTO

---
+++ Wordle/main.py
@@ -0,0 +1,24 @@
+import json
+import random
+
+# Cargar palabras desde un archivo de texto
+def cargarPalabras():
+    with open("palabras.txt", "r") as archivo:
+        palabras = [line.strip() for line in archivo]
+    return palabras
+
+# Elegir una palabra aleatoria de la lista de palabras
+def elegirPalabra(palabras):
+    return random.choice(palabras)
+
+#Crear matriz vacia de 6 filas y 5 columnas
+def crearTablero():
+    tablero[]
+    filas = 6; columnas = 5
+    
+
+# mostrar matriz vacia que va a ser el tablero del juego 
+def mostrarTablero():
+    tablero[]
+
+
