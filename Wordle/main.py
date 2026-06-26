095b3a14655de76d26b72527ecd0bb277fe9d3c1
Al0loco <alcam2004@gmail.com>
2026-06-25 20:00:29 -0600

    ADD: Se crearon funciones iniciales

--- Wordle/main.py
+++ Wordle/main.py
@@ -13,12 +13,39 @@ def elegirPalabra(palabras):
 
 #Crear matriz vacia de 6 filas y 5 columnas
 def crearTablero():
-    tablero[]
-    filas = 6; columnas = 5
+    tablero = []           # lista principal vacía
     
+    for i in range(6):     # 6 filas (intentos)
+        fila = []
+        for j in range(5): # 5 columnas (letras)
+            fila.append("_")
+        tablero.append(fila)
+    
+    return tablero
+
 
 # mostrar matriz vacia que va a ser el tablero del juego 
 def mostrarTablero():
-    tablero[]
+    print("WORDLE")
+    for i in tablero:
+        print(i)
 
 
+# Validar la palabra ingresada
+def validarEntrada(intentoUsuario):
+    # Verificar que tenga exactamente 5 letras
+    if len(intentoUsuario) != 5:
+        print("La palabra debe tener exactamente 5 letras.")
+        return False
+    
+    # Verificar que solo tenga letras del alfabeto
+    for letra in intentoUsuario:
+        if not letra.isalpha():
+            print("La palabra solo puede contener letras.")
+            return False
+    
+    return True
+
+
+def compararIntento(intentoUsuario, palabraSecreta):
+    
